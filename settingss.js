46a2d81145b40c5447e0c70a33535918f458b209
IHOYELO <ihoyelo1234@gmail.com>
2025-07-26 17:20:53 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "CRISS-AI-JFQUnJCB#IFeGJF_1Hlc22z65QN88bxNx3EKkSfvyCbJR8cBYbGc ",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "255758319605", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
